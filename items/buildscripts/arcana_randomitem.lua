2e5e0290b0fed4b9ecc02e54adab3dc5027bfe1b
SilverSokolova <80606782+SilverSokolova@users.noreply.github.com>
2021-05-19 21:53:44 -0500

    Update arcana_randomitem.lua

--- items/buildscripts/arcana_randomitem.lua
+++ items/buildscripts/arcana_randomitem.lua
@@ -1,5 +1,6 @@
 function build(_, config, _)
-  local a = root.isTreasurePool(config.pool) and config.pool or "desertTreasure"
+  local b = "desertTreasure"
+  local a = root.isTreasurePool(config.pool or b) and (config.pool or b) or b
   a = root.createTreasure(config.pool,config.level or 1)
   a = a[math.random(#a)]
   config.itemName = a.name
