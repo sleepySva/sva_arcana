a85f3b83eb1ce21643a6d35543e62f721cc2e459
sleepySva <sleepysva@gmail.com>
2023-04-25 05:08:18 -0500

    v1.2 texture adjustments

--- stats/effects/arcana_status_radiance/arcana_status_radiance.lua
+++ stats/effects/arcana_status_radiance/arcana_status_radiance.lua
@@ -5,8 +5,8 @@ function init()
   
   script.setUpdateDelta(5)
 
-  self.tickDamagePercentage = 0.05
-  self.tickTime = 2.0
+  self.tickDamagePercentage = 0.04
+  self.tickTime = 6.0
   self.tickTimer = self.tickTime
   
   world.sendEntityMessage(entity.id(), "queueRadioMessage", "arcana_status_radiance", 5.0)
@@ -20,7 +20,7 @@ function update(dt)
   self.tickTimer = self.tickTimer - dt
   if self.tickTimer <= 0 then
     self.tickTimer = self.tickTime
-	self.tickDamagePercentage = self.tickDamagePercentage + 0.1
+	self.tickDamagePercentage = self.tickDamagePercentage + 0.08
     status.applySelfDamageRequest({
         damageType = "IgnoresDef",
         damage = math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 1,
