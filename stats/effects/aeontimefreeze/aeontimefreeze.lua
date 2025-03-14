a6600b3d6713afeba2b33542857a1cbd2939df43
sleepysva <sleepysva@gmail.com>
2025-03-14 13:49:44 -0500

    Painter & hacker tenants, Aeon tile changes

--- stats/effects/aeontimefreeze/aeontimefreeze.lua
+++ stats/effects/aeontimefreeze/aeontimefreeze.lua
@@ -1,39 +1,48 @@
 function init()
-  animator.setParticleEmitterOffsetRegion("numerals", mcontroller.boundBox())
-  animator.setParticleEmitterActive("numerals", true)
-  effect.setParentDirectives("fade=6a2284=0.4")
-  animator.playSound("timefreeze_start")
-  animator.playSound("timefreeze_loop", -1)
-  animator.setAnimationRate(1)
-  effect.addStatModifierGroup({
-    {stat = "protection", amount = 999999},
-    {stat = "fireStatusImmunity", amount = 1},
-    {stat = "iceStatusImmunity", amount = 1},
-    {stat = "electricStatusImmunity", amount = 1},
-    {stat = "poisonStatusImmunity", amount = 1},
-	{stat = "physicalResistance", amount = 999999},
-	{stat = "fireResistance", amount = 999999},
-	{stat = "iceResistance", amount = 999999},
-	{stat = "electricResistance", amount = 999999},
-	{stat = "poisonResistance", amount = 999999},
-    {stat = "powerMultiplier", effectiveMultiplier = 0}
-  })
+    local elementaltypes=root.assetJson("/damage/elementaltypes.config")
+    local buffer={}
+    local resists={}
 
-  if status.isResource("stunned") then
-    status.setResource("stunned", math.max(status.resource("stunned"), effect.duration()))
-  end
+    for _,data in pairs(elementaltypes) do
+        if data.resistanceStat then
+            buffer[data.resistanceStat]=true
+        end
+    end
 
-  mcontroller.setVelocity({0, 0})
+    table.insert(resists,{stat = "protection", amount = 10000.0})
+    table.insert(resists,{stat = "powerMultiplier", amount = 0.0})
+
+    for resist,_ in pairs(buffer) do
+        table.insert(resists,{stat = resist, amount = 100.0})
+    end
+    cultistshieldhandler=effect.addStatModifierGroup(resists)
+    animator.setParticleEmitterOffsetRegion("numerals", mcontroller.boundBox())
+    animator.setParticleEmitterActive("numerals", true)
+    effect.setParentDirectives("fade=6a2284=0.4")
+    animator.playSound("timefreeze_start")
+    animator.playSound("timefreeze_loop", -1)
+    animator.setAnimationRate(1)
+    if status.isResource("stunned") then
+        status.setResource("stunned", math.min(status.resourceMax("stunned"), effect.duration()))
+    end
+    mcontroller.setVelocity({0, 0})
 end
 
-function update(dt)
-  mcontroller.setVelocity({0, 0})
-  mcontroller.controlModifiers({
-      facingSuppressed = true,
-      movementSuppressed = true
-    })
+    function update(dt)
+        mcontroller.setVelocity({0, 0})
+        mcontroller.controlModifiers({
+            facingSuppressed = true,
+            movementSuppressed = true
+        })
+    if status.isResource("stunned") then
+        status.setResource("stunned", math.min(status.resourceMax("stunned"), effect.duration()))
+    end
+    end
+
+function uninit()
+    effect.removeStatModifierGroup(cultistshieldhandler)
 end
 
 function onExpire()
-  animator.setAnimationRate(1)
+    animator.setAnimationRate(1)
 end
